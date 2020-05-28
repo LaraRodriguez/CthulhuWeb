23ccd882233b4e3de6be54eb961dc014ab7eddf3
LilithRelish <laragoblin@gmail.com>
2020-05-28 10:07:55 +0200

    buttons coded

--- main.js
+++ main.js
@@ -1,13 +1,138 @@
-const url = 'https://achtungcthulhu.herokuapp.com/skills'
+const url = 'https://achtungcthulhu.herokuapp.com'
 
-function Fetch(){
-fetch(url)
-.then(response => response.json() )
-.then(data => {
+function Fetch() {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
 
-  let element = document.getElementById('element')
-  element.innerHTML = `<p>${data.name}</p>`
-  console.log(data)
-})
-.catch(err=>console.log(err))
+      let element = document.getElementById('element')
+      element.innerHTML = `<p>${data.name}</p>`
+      console.log(data)
+    })
+    .catch(err => console.log(err))
 }
+
+function personal_button() {
+  fetch('https://achtungcthulhu.herokuapp.com/personalsheet')
+    .then(response => response.json())
+    .then(data => {
+
+      let element = document.getElementById('element')
+      element.innerHTML = `
+      <p>${data.name}</p>
+      <p>${data.description}</p>`;
+      console.log(data)
+    })
+    .catch(err => console.log(err))
+}
+
+function skill_button() {
+  fetch('http://www.dnd5eapi.co/api/spells/acid-arrow')
+    .then(response => response.json())
+    .then(data => {
+      let element = document.getElementById('element')
+      element.innerHTML = `
+      <p>${data.name}</p>
+      <p>${data.desc}</p>`;
+      console.log(data)
+    })
+    .catch(err => console.log(err))
+}
+
+function equipement_button() {
+  fetch('https://achtungcthulhu.herokuapp.com/equipement')
+    .then(response => response.json())
+    .then(data => {
+
+      let element = document.getElementById('element')
+      element.innerHTML = `
+      <p>${data.name}</p>
+      <p>${data.description}</p>`;
+      console.log(data)
+    })
+    .catch(err => console.log(err))
+}
+
+function nacionality_button() {
+  fetch('https://achtungcthulhu.herokuapp.com/nacionality')
+    .then(response => response.json())
+    .then(data => {
+
+      let element = document.getElementById('element')
+      element.innerHTML = `
+      <p>${data.name}</p>
+      <p>${data.description}</p>`;
+      console.log(data)
+    })
+    .catch(err => console.log(err))
+}
+
+function weapon_button() {
+  fetch('https://achtungcthulhu.herokuapp.com/weapons')
+    .then(response => response.json())
+    .then(data => {
+
+      let element = document.getElementById('element')
+      element.innerHTML = `
+      <p>${data.name}</p>
+      <p>${data.description}</p>`;
+      console.log(data)
+    })
+    .catch(err => console.log(err))
+}
+
+function job_button(){
+  fetch('https://achtungcthulhu.herokuapp.com/jobs')
+  .then(response => response.json() )
+  .then(data => {
+  
+    let element = document.getElementById('element')
+    element.innerHTML = `
+    <p>${data.name}</p>
+    <p>${data.description}</p>`;
+    console.log(data)
+  })
+  .catch(err=>console.log(err))
+}
+
+function mentalill_button(){
+  fetch('https://achtungcthulhu.herokuapp.com/mentalillnesses')
+  .then(response => response.json() )
+  .then(data => {
+  
+    let element = document.getElementById('element')
+    element.innerHTML = `
+    <p>${data.name}</p>
+    <p>${data.description}</p>`;
+    console.log(data)
+  })
+  .catch(err=>console.log(err))
+}
+
+function myth_button(){
+  fetch('https://achtungcthulhu.herokuapp.com/myths')
+  .then(response => response.json() )
+  .then(data => {
+  
+    let element = document.getElementById('element')
+    element.innerHTML = `
+    <p>${data.name}</p>
+    <p>${data.description}</p>`;
+    console.log(data)
+  })
+  .catch(err=>console.log(err))
+}
+
+function sanity_button(){
+  fetch('https://achtungcthulhu.herokuapp.com/sanity')
+  .then(response => response.json() )
+  .then(data => {
+  
+    let element = document.getElementById('element')
+    element.innerHTML = `
+    <p>${data.name}</p>
+    <p>${data.description}</p>`;
+    console.log(data)
+  })
+  .catch(err=>console.log(err))
+  }
